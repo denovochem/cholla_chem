860fe2bc8b8869a9af9b3e4de1b056c234d1214b
Carson Britt <carson.s.britt@gmail.com>
2025-12-09 11:06:50 -0700

    Update __all__ to include additional resolvers

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,14 @@
 """name_to_smiles initialization."""
 from .main import convert_name_to_smiles
 
-__all__ = ['convert_name_to_smiles']
+__all__ = [
+  'resolve_compounds_to_smiles',
+  'ManualNameResolver',
+  'OpsinNameResolver',
+  'PubChemNameResolver',
+  'PeptideNameResolver',
+  'StructuralFormulaNameResolver',
+  'CIRPyNameResolver',
+]
 
 __version__ = "0.0.1"
